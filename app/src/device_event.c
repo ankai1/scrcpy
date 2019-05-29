5c3b273c2050fadd40a565f964089e74a0cdc5ac
Romain Vimont <rom@rom1v.com>
2019-05-30 00:24:26 +0200

    Add clipboard device event
    Add the mechanism to support device events, and implement the first one
    to receive the clipboard content.

---
+++ app/src/device_event.c
@@ -0,0 +1,51 @@
+#include "device_event.h"
+
+#include <string.h>
+#include <SDL2/SDL_assert.h>
+
+#include "buffer_util.h"
+#include "log.h"
+
+ssize_t
+device_event_deserialize(const unsigned char *buf, size_t len,
+                         struct device_event *event) {
+    if (len < 3) {
+        // at least type + empty string length
+        return 0; // not available
+    }
+    if (buf[0] != DEVICE_EVENT_TYPE_CLIPBOARD) {
+        LOGW("Unsupported device event type: %d", (int) buf[0]);
+        return -1; // error, we cannot recover
+    }
+    switch (buf[0]) {
+        case DEVICE_EVENT_TYPE_CLIPBOARD: {
+            uint16_t clipboard_len = buffer_read16be(&buf[1]);
+            if (clipboard_len > len - 3) {
+                return 0; // not available
+            }
+            char *text = SDL_malloc(clipboard_len + 1);
+            if (!text) {
+                LOGW("Could not allocate text for clipboard");
+                return -1;
+            }
+            if (clipboard_len) {
+                memcpy(text, &buf[3], clipboard_len);
+            }
+            text[clipboard_len] = '\0';
+
+            event->type = DEVICE_EVENT_TYPE_CLIPBOARD;
+            event->clipboard_event.text = text;
+            return 3 + clipboard_len;
+        }
+        default:
+            SDL_assert(!"unreachable");
+            return -1;
+    }
+}
+
+void
+device_event_destroy(struct device_event *event) {
+    if (event->type == DEVICE_EVENT_TYPE_CLIPBOARD) {
+        SDL_free(event->clipboard_event.text);
+    }
+}
